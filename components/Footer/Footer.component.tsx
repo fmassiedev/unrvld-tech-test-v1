606c682a9a92f3ec10e5da08e335b9a643d4ec9f
fmassiedev <fmassiedev@users.noreply.github.com>
2024-12-04 18:43:02 +0000

    Footer component
    Building out the footer component

--- components/Footer/Footer.component.tsx
+++ components/Footer/Footer.component.tsx
@@ -1,5 +1,14 @@
+import Link from "next/link";
+
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="py-16 px-4 md:px-8 lg:px-16 xl:32 2xl:px-64 bg-gray-100 text-sm mt-24 flex justify-between">
+      <Link href="/" className="flex items-center gap-3">
+        <div className="text-2xl tracking-wide">LOGO</div>
+      </Link>
+      <p>&copy; Copyright</p>
+    </footer>
+  );
 };
 
 export default Footer;
