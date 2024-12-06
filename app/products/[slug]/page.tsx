2dffc5bf7fc0638614e48c9f1c8ff7498253e9d8
fmassiedev <fmassiedev@users.noreply.github.com>
2024-12-06 16:05:51 +0000

    Product Gallery and Single Product Page
    1) Product Gallery Component built with dummy images and added to Single Product Page

--- app/products/[slug]/page.tsx
+++ app/products/[slug]/page.tsx
@@ -1,10 +1,30 @@
 import PageTitle from "@/components/PageTitle/PageTitle.component";
-import React from "react";
+import ProductGallery from "@/components/Product/ProductGallery/ProductGallery.component";
 
 export default async function ProductDetailsPage() {
   return (
     <main>
       <PageTitle title="Single Product" />
+
+      <div className="container mx-auto max-w-[1200px] p-4 relative flex flex-col lg:flex-row gap-16">
+        <div className="w-full lg:w-1/2 lg:sticky top-20 h-max">
+          <ProductGallery />
+        </div>
+
+        <div className="w-full lg:w-1/2 flex flex-col gap-6">
+          <h1 className="text-4xl text-slate-800 font-bold">Product Name</h1>
+          <p className="text-slate-500">
+            Product Description - Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta
+            impedit itaque cum ipsum ullam, debitis possimus, cumque obcaecati, deleniti sequi
+            reprehenderit voluptates eum sed rem consectetur aliquam quis modi fugiat?
+          </p>
+          <div className="h-[2px] bg-slate-100" />
+          <h2 className="font-medium text-2xl">Product Price</h2>
+          <div className="h-[2px] bg-slate-100" />
+          Something with Variants?
+          <div className="h-[2px] bg-slate-100" />
+        </div>
+      </div>
     </main>
   );
 }
---
+++ components/Product/ProductGallery/ProductGallery.component.tsx
@@ -0,0 +1,52 @@
+"use client";
+
+import Image from "next/image";
+import { useState } from "react";
+
+const DUMMY_IMAGES = [
+  {
+    id: 1,
+    url: "https://cdn.shopify.com/s/files/1/0688/1755/1382/products/slides.jpg?v=1675447358",
+  },
+  {
+    id: 2,
+    url: "https://cdn.shopify.com/s/files/1/0688/1755/1382/products/GreenSweatpants01.jpg?v=1675455387",
+  },
+  {
+    id: 3,
+    url: "https://cdn.shopify.com/s/files/1/0688/1755/1382/products/GreenTshirt01.jpg?v=1675455410",
+  },
+  {
+    id: 4,
+    url: "https://cdn.shopify.com/s/files/1/0688/1755/1382/products/GreenHoodie01.jpg?v=1675455175",
+  },
+];
+
+export default function ProductGallery() {
+  const [activeImageIndex, setActiveImageIndex] = useState(0);
+
+  return (
+    <div className="">
+      <div className="aspect-w-1 aspect-h-1 relative">
+        <Image
+          src={DUMMY_IMAGES[activeImageIndex].url}
+          alt=""
+          fill
+          sizes="50vw"
+          className="object-cover rounded"
+        />
+      </div>
+      <div className="flex justify-between gap-4">
+        {DUMMY_IMAGES.map((img: { id: number; url: string }, i: number) => (
+          <div
+            className="w-1/4 h-32 relative gap-4 mt-4 cursor-pointer"
+            key={img.id}
+            onClick={() => setActiveImageIndex(i)}
+          >
+            <Image src={img.url} alt="" fill sizes="30vw" className="object-cover rounded" />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
